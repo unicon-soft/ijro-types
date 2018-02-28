b176b0888281cc9f563e99f17be929a796a52404
Farrux Mamatxalilov <mfarrukhjon@gmail.com>
2018-02-28 15:41:19 +0500

    fix

--- index.ts
+++ index.ts
@@ -4,7 +4,6 @@ export {TaskControlPlan} from "./types/TaskControlPlan"
 export {Db} from "./types/Db"
 export {DBRecipient} from "./types/DBRecipient"
 
-
 export {TaskTypeEnum} from "./types/task/TaskTypEnum"
 export {TaskListByRecipientUserRequest} from "./types/TaskListByRecipientUserRequest"
 export {TaskListByRecipientUser} from "./types/TaskListByRecipientUserResponse"
