f732362d6ad2cb0e914fed838b313cb2173fc1cc
Farrux Mamatxalilov <mfarrukhjon@gmail.com>
2018-02-28 10:43:15 +0500

    fix exports

--- index.ts
+++ index.ts
@@ -1,1 +1,7 @@
 export * from "./types"
+
+export {Task} from "./types/Task"
+export {TaskRating} from "./types/TaskRating"
+export {TaskControlPlan} from "./types/TaskControlPlan"
+export {Db} from "./types/Db"
+export {DBRecipient} from "./types/DBRecipient"
