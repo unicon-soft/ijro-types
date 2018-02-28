1b0094c34b0993e47beeb768d110d2904ad85419
Farrux Mamatxalilov <mfarrukhjon@gmail.com>
2018-02-28 15:43:43 +0500

    fix

---
+++ index.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Task_1 = require("./types/Task");
+exports.Task = Task_1.Task;
+var TaskRating_1 = require("./types/TaskRating");
+exports.TaskRating = TaskRating_1.TaskRating;
+var TaskControlPlan_1 = require("./types/TaskControlPlan");
+exports.TaskControlPlan = TaskControlPlan_1.TaskControlPlan;
+var Db_1 = require("./types/Db");
+exports.Db = Db_1.Db;
+var DBRecipient_1 = require("./types/DBRecipient");
+exports.DBRecipient = DBRecipient_1.DBRecipient;
+var TaskTypEnum_1 = require("./types/task/TaskTypEnum");
+exports.TaskTypeEnum = TaskTypEnum_1.TaskTypeEnum;
+var TaskListByRecipientUserRequest_1 = require("./types/TaskListByRecipientUserRequest");
+exports.TaskListByRecipientUserRequest = TaskListByRecipientUserRequest_1.TaskListByRecipientUserRequest;
+var TaskListByRecipientUserResponse_1 = require("./types/TaskListByRecipientUserResponse");
+exports.TaskListByRecipientUser = TaskListByRecipientUserResponse_1.TaskListByRecipientUser;
