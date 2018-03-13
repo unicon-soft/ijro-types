b0df07f495b0b69eadc6659103f6e33c11d76f33
khabibullayev <sh.xabibullayev@gmail.com>
2018-03-13 19:24:04 +0500

    Fix

---
+++ types/task/TaskByIdRequest.ts
@@ -0,0 +1,12 @@
+export class TaskById {
+    recipient_id: string;
+    db_id: string;
+    document_id: string;
+
+    constructor(params: any = {}) {
+        const {recipient_id, db_id, document_id} = params;
+        this.recipient_id = recipient_id;
+        this.db_id = db_id;
+        this.document_id = document_id;
+    }
+}
